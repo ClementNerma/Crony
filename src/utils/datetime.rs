977a72341383800f727e98e9079eba3da5b7aca8
Clément Nerma <clement.nerma@gmail.com>
2022-09-17 22:25:12 +0200

    Allow to use local offset instead of forcing UTC

--- src/utils/datetime.rs
+++ src/utils/datetime.rs
@@ -1,12 +1,23 @@
 use anyhow::Context;
-use time::{format_description, OffsetDateTime};
+use once_cell::sync::Lazy;
+use time::{format_description, OffsetDateTime, UtcOffset};
+
+use crate::warn;
+
+// Required as the offset can fail to be get in some contexts
+static OFFSET: Lazy<UtcOffset> = Lazy::new(|| {
+    UtcOffset::local_offset_at(OffsetDateTime::now_utc()).unwrap_or_else(|_| {
+        warn!("Failed to determine local offset, UTC will be used instead");
+        UtcOffset::UTC
+    })
+});
 
 pub fn get_now() -> OffsetDateTime {
     // OffsetDateTime::now_local()
     //     .context("Failed to determine current date/time")
     //     .unwrap()
 
-    OffsetDateTime::now_utc()
+    OffsetDateTime::now_utc().to_offset(*OFFSET)
 }
 
 pub fn get_now_second_precision() -> OffsetDateTime {
