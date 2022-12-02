16a853e12a811d2460aee98c96c5b9aa7b54f138
Clément Nerma <clement.nerma@gmail.com>
2022-12-02 16:45:16 +0100

    Show detailed help when running the program

--- src/cmd.rs
+++ src/cmd.rs
@@ -5,7 +5,7 @@ use clap::{Args, Parser, Subcommand};
 use crate::daemon::DaemonStartArgs;
 
 #[derive(Parser)]
-#[clap(author, version, help = "Replacement program for 'cron' and 'crontab'")]
+#[clap(version, about, author)]
 pub struct Cmd {
     #[clap(short, long, help = "Path to the data directory")]
     pub data_dir: Option<PathBuf>,
